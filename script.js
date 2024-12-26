7d99a642196a5702b3bb8373a559e724d9553ab7
Spencer Rauch <rauchs@gmail.com>
2024-12-26 10:58:45 -0800

    modes are important

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const modeCheck = document.getElementById("modeCheck")
+const body = document.querySelector("body");
 
 
 function toggle(){
     console.log(modeCheck.checked);
+    if (modeCheck.checked){
+        body.classList.add("light")
+        body.classList.remove("dark")
+    } else {
+        body.classList.add("dark")
+        body.classList.remove("light")
+    }
 }

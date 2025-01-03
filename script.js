9f4f99792c0c275bdac499ef0ba1f57a2b029721
Spencer Rauch <rauchs@gmail.com>
2025-01-03 10:26:53 -0800

    contact section, about updated

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ const modeCheck = document.getElementById("modeCheck")
 const html = document.querySelector('html');
 const body = document.querySelector("body");
 const initialDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const contactIcons = document.querySelectorAll(".contact_icon");
 
 if (!initialDark){
     modeCheck.checked = true;
 }
 
+console.log(contactIcons.length);
+
+contactIcons.forEach(icon => {
+    icon.addEventListener("mouseenter", () => {
+        console.log("entered");
+        console.log(icon.nextElementSibling);
+        icon.nextElementSibling.style.setProperty("display", "block");
+    });
+});
+
+contactIcons.forEach(icon => {
+    icon.addEventListener("mouseleave", () => {
+        icon.nextElementSibling.style.setProperty("display", "none");
+    });
+});
 
 function toggle(){
     if (modeCheck.checked){
@@ -20,3 +36,4 @@ function toggle(){
 function linkFunc(element){
     element.scrollIntoView({behavior:"smooth"});
 }
+
